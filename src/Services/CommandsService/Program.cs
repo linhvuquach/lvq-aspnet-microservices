44eeabf5aab9b21430c354a34277178e1f3536f3
linh.vu.quach <linh.quach@netpower.no>
2023-04-12 18:05:44 +0700

    feat: Add Event Bus Subcriber to Command service, and testing workflow Sync communicatioin

--- src/Services/CommandsService/Program.cs
+++ src/Services/CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddHostedService<MessageBusSubcriber>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessing>();
 
 builder.Services.AddControllers();
