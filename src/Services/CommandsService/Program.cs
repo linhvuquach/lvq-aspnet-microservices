bc1b3a82cff23e1697bce7778fa438e3efaf8f0e
linh.vu.quach <linh.quach@netpower.no>
2023-04-14 10:38:33 +0700

    feat: adding a gRPC client to Commands Service

--- src/Services/CommandsService/Program.cs
+++ src/Services/CommandsService/Program.cs
@@ -1,6 +1,7 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddHostedService<MessageBusSubcriber>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessing>();
+builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,5 +36,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPolulation(app);
 
 app.Run();
